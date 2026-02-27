67e18b45ee5995489c60c5e635dfdc3e3c8ab2ee
FilinDmitry <filin.dmitry07@gmail.com>
2026-02-27 17:36:42 +0300

    Вычисление графика + некоторые базовые проверки

--- Практическая работа 4 Филин Березнев/pages/it_pages3.xaml.cs
+++ Практическая работа 4 Филин Березнев/pages/it_pages3.xaml.cs
@@ -20,23 +20,103 @@ namespace Практическая_работа_4_Филин_Березнев.pa
     
 public partial class it_pages3 : Page
     {
-
+        List<TextBox> textBoxes;
         public it_pages3()
         {
             InitializeComponent();
-            
+            textBoxes = new List<TextBox> { b_znach, x0_znach, xk_znacht, dx_znach, otvet };
+            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
 
-            
+            Series currentSeries = new Series("Платежи")
+            {
+                IsValueShownAsLabel = true
+            };
+            ChartPayments.Series.Add(currentSeries);
+            currentSeries.IsValueShownAsLabel = false;
+            currentSeries.ChartType = SeriesChartType.Spline;
+
         }
 
         private void vichislit_Click(object sender, RoutedEventArgs e)
         {
+            
+            Series currentSeries = ChartPayments.Series.FirstOrDefault();
+            currentSeries.Points.Clear();
+            double b, dx, x0, xk;
+            bool b_ = false, dx_ = false, x0_ = false, xk_ = false;
+            
+            b_ = double.TryParse(b_znach.Text, out b);
+            dx_ = double.TryParse(dx_znach.Text, out dx);
+            x0_ = double.TryParse(x0_znach.Text, out x0);
+            xk_ = double.TryParse(xk_znacht.Text, out xk);
+            if (!b_ || !dx_ || !x0_ || !xk_)
+            {
+                MessageBox.Show("Ты ошибся тестировщик, баг в другом замке \n\nP.S. Если что ввод дробного числа через запятую");
+                return;
+            }
+                
+            if (dx == 0)
+            {
+                MessageBox.Show("атата, чо думал самый умный, dx = 0 низя");
+                return;
+            }
+
+            if ((dx > 0 && x0 > xk) || (dx < 0 && x0 < xk))
+            {
+                MessageBox.Show("Ты хочешь войти в бесконечный цикл, а ещё чего? Ищи баги в другом месте");
+                return;
+            }
+
+            try
+            {
+                if (x0 > xk)
+                {
+                    while (x0 >= xk)
+                    {
+                        if (b != x0)
+                        {
+                            currentSeries.Points.AddXY(x0, Formula.third_func(x0, b));
+                        }
+                        else
+                        {
+                            currentSeries.Points.AddXY(x0 - 0.001, Formula.third_func(x0 - 0.001, b));
+                            currentSeries.Points.AddXY(x0 + 0.001, Formula.third_func(x0 + 0.001, b));
+                        }
+                            x0 += dx;
+                    }
+                }
+                else
+                {
+                    while (x0 <= xk)
+                    {
+                        if (b != x0)
+                        {
+                            currentSeries.Points.AddXY(x0, Formula.third_func(x0, b));
+                        }
+                        else
+                        {
+                            currentSeries.Points.AddXY(x0 - 0.001, Formula.third_func(x0 - 0.001, b));
+                            currentSeries.Points.AddXY(x0 + 0.001, Formula.third_func(x0 + 0.001, b));
+                        }
+                        x0 += dx;
+                    }
+                }
+            }
+            catch { MessageBox.Show("Что-то пошло не так в вычислениях"); }
 
         }
 
         private void otchistit_Click(object sender, RoutedEventArgs e)
         {
+            foreach (TextBox tb in textBoxes)
+            {
+                tb.Text = "";
+            }
+        }
 
+        private void UpdateChart(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
