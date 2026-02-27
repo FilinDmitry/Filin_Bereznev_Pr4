d543ae9d7aaac52d3602321cb233011830722618
Lavaus <Iaroslavbereznev2019@gail.com>
2026-02-27 13:56:40 +0300

    Создан интерфейс первого задание

---
+++ Практическая работа 4 Филин Березнев/pages/it_pages3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_работа_4_Филин_Березнев.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для it_pages3.xaml
+    /// </summary>
+    public partial class it_pages3 : Page
+    {
+        public it_pages3()
+        {
+            InitializeComponent();
+        }
+    }
+}
