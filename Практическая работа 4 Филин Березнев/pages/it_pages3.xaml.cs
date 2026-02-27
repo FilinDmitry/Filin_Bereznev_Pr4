62210888966283f5a8369df5c8c291283289aab4
Lavaus <Iaroslavbereznev2019@gail.com>
2026-02-27 15:13:22 +0300

    Диаграмма для третьего задания

--- Практическая работа 4 Филин Березнев/pages/it_pages3.xaml.cs
+++ Практическая работа 4 Филин Березнев/pages/it_pages3.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,14 +17,24 @@ using System.Windows.Shapes;
 
 namespace Практическая_работа_4_Филин_Березнев.pages
 {
-    /// <summary>
-    /// Логика взаимодействия для it_pages3.xaml
-    /// </summary>
-    public partial class it_pages3 : Page
+    
+public partial class it_pages3 : Page
     {
+
         public it_pages3()
         {
             InitializeComponent();
+            ChartPayments.ChartAreas.Add(new ChartArea("main"));
+
+            
+        }
+
+
+        private void UpdateChart(object sender, SelectionChangedEventArgs e)
+        {
+           
         }
     }
 }
+
+
