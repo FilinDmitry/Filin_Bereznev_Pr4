3b8222716f8cd3f0b69fc0a436d1a6c3cc18bc55
FilinDmitry <filin.dmitry07@gmail.com>
2026-03-20 15:06:34 +0300

    InvalidInputTests

--- TestingProject/FormulaTest.cs
+++ TestingProject/FormulaTest.cs
@@ -4,11 +4,36 @@ using Практическая_работа_4_Филин_Березнев;
 
 namespace TestingProject
 {
+    /// <summary>
+    /// Тестовый класс тестирующий вычисление значений в первой формуле (все значиния уже прошли фильтрацию и являются валидными)
+    /// </summary>
     [TestClass]
-    public class FormulaTest
+    public class FirstFormulaTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstTest()
+        {
+        }
+    }
+    /// <summary>
+    /// Тестовый класс тестирующий вычисление значений во второй формуле (все значиния уже прошли фильтрацию и являются валидными)
+    /// </summary>
+    [TestClass]
+    public class SecondFormulaTest
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+        }
+    }
+    /// <summary>
+    /// Тестовый класс тестирующий вычисление значений в третей формуле (все значиния уже прошли фильтрацию и являются валидными)
+    /// </summary>
+    [TestClass]
+    public class ThirdFormulaTest
+    {
+        [TestMethod]
+        public void FirstTest()
         {
         }
     }
--- TestingProject/InvalidInputTest.cs
+++ TestingProject/InvalidInputTest.cs
@@ -6,14 +6,117 @@ using Практическая_работа_4_Филин_Березнев.pages;
 namespace TestingProject
 {
     [TestClass]
-    public class InvalidInputTest
+    public class InvalidInputTestPage1
     {
+        it_page1 page = new it_page1();
         [TestMethod]
-        public void TestMethod1()
+        public void FirstPageDevisionByZero()
         {
-            it_page1 page = new it_page1();
-            Assert.AreEqual(page.a(), 0);
-            
+            Assert.AreEqual(page.check_valid_data("-1", "1", "0") , 1);
+        }
+        [TestMethod]
+        public void FirstPageStringInput()
+        {
+            Assert.AreEqual(page.check_valid_data("0,3", "string", "20"), 2);
+        }
+        [TestMethod]
+        public void FirstPageBlankInput()
+        {
+            Assert.AreEqual(page.check_valid_data(" ", "1", " "), 2);
+        }
+        [TestMethod]
+        public void FirstPageNoInput()
+        {
+            Assert.AreEqual(page.check_valid_data("", "", ""), 2);
+        }
+        [TestMethod]
+        public void FirstPageValidDataInput()
+        {
+            Assert.AreEqual(page.check_valid_data("1", "6", "3,4"), 0);
+        }
+    }
+
+    [TestClass]
+    public class InvalidInputTestPage2
+    {
+        it_page2 page = new it_page2();
+        [TestMethod]
+        public void SecondPageStringInput()
+        {
+            Assert.AreEqual(page.check_valid_data("1,3", "help", 2), 1);
+        }
+        [TestMethod]
+        public void SecondPageBlankInput()
+        {
+            Assert.AreEqual(page.check_valid_data(" ", "12", 1), 1);
+        }
+        [TestMethod]
+        public void SecondPageNoInput()
+        {
+            Assert.AreEqual(page.check_valid_data("12", "", 3), 1);
+        }
+        [TestMethod]
+        public void SecondPageValidDataInput()
+        {
+            Assert.AreEqual(page.check_valid_data("1", "6", 1), 0);
+        }
+    }
+
+    [TestClass]
+    public class InvalidInputTestPage3
+    {
+        // Если в наименовании теста содержиться UP это значит что dx > 0
+        // Если в наименовании теста содержиться Down это значит что dx < 0
+        it_pages3 page = new it_pages3();
+        [TestMethod]
+        public void ThirdPageStringInput()
+        {
+            Assert.AreEqual(page.check_valid_data("you", "can't", "type", "string"), 1);
+        }
+        [TestMethod]
+        public void ThirdPageBlankInput()
+        {
+            Assert.AreEqual(page.check_valid_data(" ", "1", "0,3", "30"), 1);
+        }
+        [TestMethod]
+        public void ThirdPageNoInput()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "", "13", "13"), 1);
+        }
+        [TestMethod]
+        public void ThirdPageDxEqualZero()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "0", "13", "17"), 2);
+        }
+        [TestMethod]
+        public void ThirdPageInfinityCycleUp()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "1", "17", "11"), 3);
+        }
+        [TestMethod]
+        public void ThirdPageInfinityCycleDown()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "-1", "13", "27"), 3);
+        }
+        [TestMethod]
+        public void ThirdPageDxToBigUp()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "10", "13", "20"), 4);
+        }
+        [TestMethod]
+        public void ThirdPageDxToBigDown()
+        {
+            Assert.AreEqual(page.check_valid_data("3", "-10", "20", "17"), 4);
+        }
+        [TestMethod]
+        public void ThirdPageValidDataInputUp()
+        {
+            Assert.AreEqual(page.check_valid_data("10", "0,2", "1", "5"), 0);
+        }
+        [TestMethod]
+        public void ThirdPageValidDataInputDown()
+        {
+            Assert.AreEqual(page.check_valid_data("0", "-0,5", "10", "-10"), 0);
         }
     }
 }
